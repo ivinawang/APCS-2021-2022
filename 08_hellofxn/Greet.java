698bec635d29826287b565a33469b10afa64592b
Ivina Wang <ivinaw@gmail.com>
2021-09-30 13:18:06 -0400

    classwork

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,15 @@
+//Ivina Wang
+//APCS
+//HW08 -- Refactor Freshie Zero
+//2021-09-29
+
+public class Greet {
+	public static void main(String[] args){
+		System.out.println("No hablo queso.");
+		greet("hi")
+	}
+	public static void greet(String s){
+		System.out.println(s)
+	}
+}
+
