4be7b00cd6cfbdd7a0dcd7cf59ba9160e2206eeb
ivinawang <90671844+ivinawang@users.noreply.github.com>
2021-09-30 21:55:33 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -9,6 +9,7 @@
 
 		UNRESOLVED MYSTERIES:
 		- Should we use random in implementing greet()? Our team numbers had separate opinions about this question.
+			^^ not sure how to do this too!! :/
 		- Without the use of random, our idea was to use if statements and case on the value of string s. However, this approach pre-determines which person will always be greeted, and the argument in the function call must be modified to change the person greeted.*/
 
 
