4267ebaa7cd9bfb3079d0f50a365017c2e0501f4
Ivina Wang <ivinaw@gmail.com>
2021-09-30 21:54:37 -0400

    idk hw

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -3,13 +3,23 @@
 //HW08 -- Refactor Freshie Zero
 //2021-09-29
 
+/*DISCOVERIES:
+		- We reached the conclusion that “hardcoded” means pre-programmed into the function.
+		- We reviewed information about random from our summer prework, since we considered implementing random in the function greet();
+
+		UNRESOLVED MYSTERIES:
+		- Should we use random in implementing greet()? Our team numbers had separate opinions about this question.
+		- Without the use of random, our idea was to use if statements and case on the value of string s. However, this approach pre-determines which person will always be greeted, and the argument in the function call must be modified to change the person greeted.*/
+
+
+
 public class Greet {
 	public static void main(String[] args){
-		System.out.println("No hablo queso.");
-		greet("hi")
+		greet("Foo");
+		greet("Moo");
+		greet("Ooo")
 	}
 	public static void greet(String s){
-		System.out.println(s)
+		System.out.println("Why, hello there, " + s + ". How do you do?")
 	}
 }
-
