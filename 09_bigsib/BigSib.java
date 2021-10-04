ab83078cdcbce0b452596aa55d187d5f0c8fc4fc
Ivina Wang <ivinaw@gmail.com>
2021-10-04 01:10:10 -0400

    homework 09

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,1 +1,12 @@
-static class BigSib
+/*
+LIR - Lawrence Joa (Steve and John), Ivina Wang (Sealie), Ruiwen Tang (Bernard)
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-01
+*/
+
+public class BigSib{
+  public static void greet(String s){
+    System.out.println("Why, hello there, " + s + ". How do you do?");
+  }
+}
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,3 +1,18 @@
+/*
+LIR - Lawrence Joa (Steve and John), Ivina Wang (Sealie), Ruiwen Tang (Bernard)
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-01
+
+DISCOVERIES:
+		- you can use separate java files to execute diff codes?
+		- after you javac Greet.java you type java Greet/ without the .java
+		- if you get the "error: reached end of file while parsing" it means missing/extra bracket
+
+		UNRESOLVED MYSTERIES:
+		- n/a
+*/
+
 public class Greet{
 	public static void main(String[] args){
 		BigSib.greet("Film");
