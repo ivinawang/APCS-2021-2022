45b6701c852d9f0cb2f5b645da20dcf1a64fa304
Ivina Wang <ivinaw@gmail.com>
2021-12-22 13:19:31 -0500

    cw 49

---
+++ 49_rat/Driver.java
@@ -0,0 +1,27 @@
+/**
+TNPG: Frogs (Ivina Wang, Ruby Friedman, and Samantha Hua)
+APCS
+HW49-- Rational Standards Compliance
+2021-12-22
+time spent:
+
+DISCO:
+ *
+
+QCC:
+ *
+
+**/
+
+public class Driver implements Comparable{
+  public static void main(String[] args){
+
+    //not the same type
+    //throw new ClassCastException ("\ncompateTo() input not a Rational :(");
+
+    //typecast compareTo(Object o){
+    //  Rational r = (Rational)o;
+  //  }
+
+  }
+}
