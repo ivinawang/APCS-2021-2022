27a66761d88a34ed0b2eb94d9baaef502d4fc803
Ivina Wang <ivinaw@gmail.com>
2022-04-05 22:53:06 -0400

    cobo

---
+++ cobo/100_data/DataStudentCode/Welcome03_List.java
@@ -0,0 +1,40 @@
+/*
+ * Arrays of objects
+ */
+
+import core.data.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Welcome03_List {
+   public static void main(String[] args) {
+      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
+             "station/station_id", "station/state",
+             "station/latitude", "station/longitude");
+      System.out.println("Total stations: " + allstns.size());
+
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a state abbreviation: ");
+      String state = sc.next();
+      System.out.println("Stations in " + state);
+
+      WeatherStation lowest = allstns.get(0);
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+           lowest = ws;
+           break;
+         }}
+      for (WeatherStation ws : allstns) {
+         if (ws.isLocatedInState(state)) {
+            System.out.println("  " + ws.getId() + ": " + ws.getName() + " at latitude: " + ws.getLatitude());
+            if (ws.getLatitude() < lowest.getLatitude()){
+              lowest = ws;
+            }
+         }
+
+      }
+      System.out.println("The southernmost weather station in " + state + " is " + lowest.getId());
+
+   }
+}
