4879be81a034693e09f3250503cd1863678f97d7
Ivina Wang <ivinaw@gmail.com>
2022-04-11 20:59:07 -0400

    hw 89

---
+++ 89_queue/LLQueue.java
@@ -0,0 +1,49 @@
+/*
+Team A Random Word (Nina Jiang, Orion Roven, Ivina Wang )
+APCS pd7
+HW89-- Queue Two Ways
+2022-04-11t
+time spent: .8 hrs
+*/
+
+/*
+DISCO:
+* It's much easier to code after drawing the encapsulation diagrams + looking at the api
+* We do not need a _front, _end, or _size for LLQ or ALQ
+* LinkedList has both peekFirst() and getFirst(); however peekFirst is more convenient
+  because peekFirst() returns null when the list is empty, while getFirst() throws a NoSuchElementException
+
+QCC:
+* Is this more effective than RQueue?
+* Do we need a size variable for ALQ?
+*/
+
+import java.util.LinkedList;
+
+public class LLQueue<T> implements Queue<T> {
+  private LinkedList<T> list;
+
+  public LLQueue() {
+    list = new LinkedList<T>();
+  }
+
+  public T dequeue() {
+    if(!isEmpty()) {
+      return list.removeFirst();
+    }
+    return null;
+  }
+
+  public void enqueue( T x ){
+    list.addLast(x); //right?
+  } //O(1)
+
+  public boolean isEmpty(){
+    return list.size() == 0;
+  }
+
+  public T peekFront(){
+    return list.peekFirst();
+  }
+
+}
